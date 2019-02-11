ce8678b99fb9897710f9a4ce36f18c4add96cae8
Soliman Achahbar <soliman.achahbar@gmail.com>
2019-02-11 11:21:26 +0100

    Parcours repertoire en profondeur

--- ContenuRepertoire/src/fr/unice/soliman/Main.java
+++ ContenuRepertoire/src/fr/unice/soliman/Main.java
@@ -1,9 +1,24 @@
 package fr.unice.soliman;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Main m = new Main();
+        m.lister(new File("C:\\Users\\Soliman\\Documents\\Scolaire\\Master 1\\Java\\tp1\\ContenuRepertoire"));
     }
 
+    private void lister(File rep) {
+        System.out.println (rep.toString());
+
+        if (rep.isDirectory()) {
+            File[] listFiles = rep.listFiles();
+
+            for ( int i = 0; i < listFiles.length; i++) {
+                // appel récursif
+                lister( listFiles[i]);
+            }
+        }
+    }
 }
