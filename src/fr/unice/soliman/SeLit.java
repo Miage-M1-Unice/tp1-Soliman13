8101b0bef21bc7d2042e3ec66ee62276a94cf8eb
Soliman Achahbar <soliman.achahbar@gmail.com>
2019-02-17 21:30:08 +0100

    refactor + lecture multiple fichiers ok

--- src/fr/unice/soliman/SeLit.java
+++ src/fr/unice/soliman/SeLit.java
@@ -1,18 +1,24 @@
 package fr.unice.soliman;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class SeLit {
 
-	private String sep;
+	private File repertoire_de_depart;
+	private Pattern pattern;
+
 	public SeLit() {
-		this.sep = System.getProperty("file.separator");
+		this.repertoire_de_depart = new File(System.getProperty("user.dir") + "\\src\\fr\\unice\\soliman"); // répertoire des fichiers sources java
+		this.pattern = Pattern.compile(".*\\.java");
 	}
 
-	public void setup() {
+	public void getSingleFile() {
 		InputStream is;
 		try {
 			is = new FileInputStream(System.getProperty("user.dir") + "\\src\\fr\\unice\\soliman\\SeLit.java");
@@ -24,17 +30,45 @@ public class SeLit {
 		}
 	}
 
+	public void getMultipleFiles() {
+		InputStream is;
+		File[] files = repertoire_de_depart.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return Pattern.matches(pattern.pattern(), name);
+			}
+		});
+		for (File file : files) {
+			System.out.println("\n -- Nouveau fichier -- \n");
+			if(file.isFile()) {
+				try {
+					is = new FileInputStream(file);
+					Scanner scanner = new Scanner(is);
+					lecture(scanner);
+				} catch (FileNotFoundException e) {
+					System.err.println("Fichier non trouvé");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 	void lecture(Scanner source) {
-		while(source.hasNextLine()) {  
+		while(source.hasNextLine()) {
 			String s = source.nextLine();
 			if(!s.trim().startsWith("//")) {
 				System.out.println("LU: "+s);
 			}
-		}  
-	}  
+		}
+	}
+
+	private void go() {
+		//		getSingleFile();
+		getMultipleFiles();
+	}
 
 	static public void main(String[] args) {   
 		SeLit sl = new SeLit();
-		sl.setup();
+		sl.go();
 	}
 }
